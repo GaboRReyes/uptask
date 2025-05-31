037b271652509b4641b89342b97408a71306645d
GaboRReyes <160171479+GaboRReyes@users.noreply.github.com>
2025-05-31 10:20:31 -0600

    Creacion de los primeros proyectos dentro de la base de datos
    Se agrego el metodo CreateProject para la creacion de objetos en la base de datos

--- uptask_backend/src/controllers/ProjectController.ts
+++ uptask_backend/src/controllers/ProjectController.ts
@@ -1,8 +1,22 @@
 import type { Request, Response } from 'express';
+import Project from '../models/project';
 
 
 export class ProjectController {
+
+    static createProjects = async (req: Request, res: Response) => {
+        const project = new Project(req.body);
+
+        try {
+        await project.save();
+        res.send('Proyecto creado correctamente');
+        } catch (error) {
+            console.error(error);
+    }
+}
+
     static getAllProjects = async (req: Request, res: Response) => {
         res.send('Todos los proyectos');
     }
+    
 }
