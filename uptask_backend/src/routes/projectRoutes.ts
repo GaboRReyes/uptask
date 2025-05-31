a953f46b6bae7800bb675d48754b71427b05fca2
GaboRReyes <160171479+GaboRReyes@users.noreply.github.com>
2025-05-31 11:15:21 -0600

    Creacion de endpoints
    Manejamos el CRUD de los proyectos mediante endpoints y sus correspondientes validaciones

--- uptask_backend/src/routes/projectRoutes.ts
+++ uptask_backend/src/routes/projectRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import {body} from 'express-validator'
+import {body,param} from 'express-validator'
 import { ProjectController } from '../controllers/ProjectController';
 import { handleInputErrors } from '../middleware/validation';
 
@@ -16,5 +16,30 @@ router.post('/',
     ProjectController.createProjects
 )
 router.get('/', ProjectController.getAllProjects)
+
+router.get('/:id',
+    param('id').isMongoId().withMessage('El id del proyecto no es valido'),
+    handleInputErrors,
+     ProjectController.getProjectById
+    )
+
+router.put('/:id',
+    param('id').isMongoId().withMessage('El id del proyecto no es valido'),
+    body('projectName')
+        .notEmpty().withMessage('El nombre del proyecto es obligatorio'),
+    body('clientName')
+        .notEmpty().withMessage('El nombre del cliente es obligatorio'),
+    body('description')
+        .notEmpty().withMessage('La descripcion del proyecto es obligatorio'),
+    handleInputErrors,
+     ProjectController.updateProject
+    )   
+
+router.delete('/:id',
+    param('id').isMongoId().withMessage('El id del proyecto no es valido'),
+    handleInputErrors,
+     ProjectController.deleteProject
+    )
+
 
 export default router;
